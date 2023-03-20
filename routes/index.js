1f2f4210822b3cbe3ed8dfe3d96f7732da493b03
terenzzzz <terence592592@gmail.com>
2023-03-20 18:30:59 +0000

    onboarding

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('onBoarding');
+});
+
+router.get('/index', function(req, res, next) {
   res.render('index');
 });
 
