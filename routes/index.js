57e7bcda4dc24c7b512cb1cd30786d03ab48e8ba
ozguraksay <ozgur_aksay@outlook.com>
2023-03-27 01:23:44 +0100

    Bird Page Added

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var controller = require('../controller/create')
-const {log} = require("debug");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('onBoarding');
+    res.render('onBoarding');
 });
 
 router.get('/index', function(req, res, next) {
-  res.render('index');
+    res.render('index');
 });
-
-
-// For testing add sighting to mongoDB
-router.get('/add', function(req, res, next) {
-  controller.createSighting(req,res)
-      .then(function(insertedId) {
-        console.log(insertedId)})
-      .catch(function(err) {
-        console.error(err);
-      });
-})
-
 router.get('/create', function(req, res, next) {
-  res.render('create');
+    res.render('create');
 });
+router.get('/bird', function(req, res, next) {
+    res.render('bird');
+});
+
+
 module.exports = router;
