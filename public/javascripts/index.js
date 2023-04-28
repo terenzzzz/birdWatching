77bbbbef669d34d2221fd4aa00ab6267675c913f
terenzzzz <terence592592@gmail.com>
2023-04-28 16:38:40 +0100

    Can sort by location

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -16,6 +16,7 @@ function updateSightings(sightings){
               <h1>${obj.identification}</h1>
               <p class="m-0 p-0">Seen on ${obj.dateTime}</p>
               <p class="m-0 p-0">By ${obj.nickName}</p>
+              <p class="m-0 p-0 text-primary">${parseFloat(obj.latitude).toFixed(2)},${parseFloat(obj.longitude).toFixed(2)}</p>
             </div>
             <div class="col-2 d-flex justify-content-center align-items-center">
               <a href="/bird/${obj._id}"><i class="bi bi-arrow-right" style="font-size: 4rem"></i></a>
@@ -40,11 +41,39 @@ function sortByDate(data){
 }
 
 function sortByLocation(data){
-    // [
-//     {nickName: 'Terenzzzz',  dateTime: '27-04-2023', }
-//     {nickName: 'Terenzzzz',  dateTime: '26-04-2023', }
-//     {nickName: 'Terenzzzz',  dateTime: '22-04-2023', }
-//     {nickName: 'Terenzzzz',  dateTime: '24-04-2023', }
-// ]
-    console.log(data)
+    if (navigator.geolocation) {
+        // Get Current Location
+        navigator.geolocation.getCurrentPosition(function(position) {
+            console.log(`Latitude: ${position.coords.latitude}, Longitude: ${position.coords.longitude}`);
+
+            // id:Distance
+            var idDis =  {};
+            var currentLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+            // Calculate Distance and add to dictionary
+            data.forEach(st => {
+                console.log(st)
+                var point2 = new google.maps.LatLng(st.latitude, st.longitude);
+                var distance = google.maps.geometry.spherical.computeDistanceBetween(currentLocation, point2);
+                // turn into km
+                var distanceInKm = distance / 1000;
+                idDis[st._id] = distanceInKm;
+            });
+            // Sort Dictionary by distance
+            const sortedDict = Object.entries(idDis).sort((a, b) => a[1] - b[1]);
+
+
+            // Sorted Sightings Object
+            var sorted = []
+            sortedDict.forEach(st=>{
+                sorted.push(data.find(item => item._id === st[0]))
+            })
+            updateSightings(sorted)
+        }, function(error) {
+            console.log("Error getting current position: " + error.message);
+        });
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+    }
+
 }
