4ea51163767a5503f8cb04c68466c5f560fba609
terenzzzz <terence592592@gmail.com>
2023-05-09 01:12:31 +0100

    fixed

---
+++ public/javascripts/add.js
@@ -0,0 +1,36 @@
+function sendAjaxQuery(url, data) {
+    $.ajax({
+        url: url ,
+        data: data,
+        dataType: 'json',
+        type: 'POST',
+        processData: false,
+        contentType: false,
+        success: function (dataR) {
+            // no need to JSON parse the result, as we are using
+            // dataType:json, so JQuery knows it and unpacks the
+            // object for us before returning it
+            var ret = dataR;
+            // in order to have the object printed by alert
+            // we need to JSON stringify the object
+            document.getElementById('results').innerHTML= JSON.stringify(ret);
+        },
+        error: function (xhr, status, error) {
+
+            alert('Error: ' + error.message);
+        }
+    });
+}
+
+function onSubmit() {
+
+    event.preventDefault();
+    event.stopImmediatePropagation();
+    var myForm = document.getElementById('addForm');
+    var formData = new FormData(myForm);
+    console.log(formData);
+    sendAjaxQuery('/create', formData);
+    return false;
+}
+
+
---
+++ public/javascripts/indexDb.js
@@ -0,0 +1,74 @@
+let requestIndexedDB = indexedDB.open("birdWatching",2)
+
+
+requestIndexedDB.addEventListener("error",handlerError)
+requestIndexedDB.addEventListener("upgradeneeded",upgradeStores)
+requestIndexedDB.addEventListener("success", handleSuccess)
+
+
+
+function insertSighting (data,id){
+    console.log("insertSighting to indexDB")
+    const birtWatchingIDB = requestIndexedDB.result
+    const transaction = birtWatchingIDB.transaction(["sighting"],"readwrite")
+    const sightingStore = transaction.objectStore("sighting")
+
+    data._id = id
+    console.log(data)
+
+    const addRequest = sightingStore.add(data)
+    addRequest.onsuccess = (event) => {
+        console.log("New sighting added to database with id:", event.target.result)
+        window.location.href = "/index";
+    }
+    addRequest.onerror = (event) => {
+        console.error("Error adding new sighting to database:", event.target.error)
+    }
+}
+
+function getSighting() {
+    return new Promise((resolve, reject) => {
+        const birtWatchingIDB = requestIndexedDB.result;
+        const transaction = birtWatchingIDB.transaction(["sighting"], "readwrite");
+        const sightingStore = transaction.objectStore("sighting");
+        const cursorRequest = sightingStore.openCursor();
+        const result = []; // 存储查询结果的数组
+
+        cursorRequest.onsuccess = function(event) {
+            const cursor = event.target.result;
+            if (cursor) {
+                const data = cursor.value;
+                result.push(data); // 将对象数据添加到数组中
+                cursor.continue();
+            } else {
+                // 遍历完所有对象，使用 Promise 的 resolve 返回结果
+                resolve(result);
+            }
+        };
+
+        cursorRequest.onerror = function(event) {
+            // 处理错误，使用 Promise 的 reject 返回错误信息
+            reject(event.target.error);
+        };
+    });
+}
+
+
+
+
+
+
+function handlerError(err){
+    console.log(`IndexDb Error: ${err}` )
+}
+
+function upgradeStores(ev){
+    const db = ev.target.result
+    db.createObjectStore("sighting",{keyPath:"id", autoIncrement : true})
+    console.log("Object:'Sighting' created in upgradeStores" )
+}
+
+function handleSuccess(ev){
+    // init
+    console.log("IndexDb Success" )
+}
